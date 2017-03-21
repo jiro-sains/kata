512cea0f6bf9d84b377ac6e83851949ac04465e2
jiro-sains <jrosains@gmail.com>
2017-03-21 01:03:28 +0000

    more work on reverse list

--- linkedlist4.rb
+++ linkedlist4.rb
@@ -23,31 +23,45 @@ def reverse_list(list, previous = nil)
     puts list.value
     if previous == nil
       puts "previous is nil"
-      #list.next_node = nil
+      temp = list.next_node
+      list.next_node = nil
       previous = list
-      puts "list.next_node: #{list.next_node.value} previous after: #{previous.value}"
-      reverse_list(list.next_node, previous)
+      puts "temp: #{temp.value} previous after: #{previous.value}"
+      reverse_list(temp, previous)
     else 
       puts "second time previous is not nil"
-      previous = list 
+   
       #list.next_node = previous   
-      puts "list.next_node: #{list.next_node.value} previous after: #{previous.value}"
-      reverse_list(list.next_node, previous)
+      puts "list.value: #{list.value} previous: #{previous.value}"
+      if list.next_node != nil
+        temp = list.next_node
+        list.next_node = previous
+        puts "list.next_node: #{temp.value} list: #{list.value}"
+        reverse_list(temp, list)
+      else
+        return
+      end
     end
   else
     return
   end
 end
 
-node1 = LinkedListNode.new(37)
-node2 = LinkedListNode.new(99, node1)
-node3 = LinkedListNode.new(12, node2)
+node1 = LinkedListNode.new(1)
+node2 = LinkedListNode.new(2, node1)
+node3 = LinkedListNode.new(3, node2)
 
 print_values(node3)
+print_values(node2)
+print_values(node1)
 puts "-----"
 reverse_list(node3)
 #print node1.value
 #print node2.value
 #print node3.value
 print_values(node3)
+print_values(node2)
+print_values(node1)
+
+
 
