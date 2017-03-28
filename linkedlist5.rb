62263d920f805c4aaef28c611d52c04637ac7dcd
Jacqueline Rosains <jrosains@gmail.com>
2017-03-28 20:52:48 +0000

    refactoring with Piotr

--- linkedlist5.rb
+++ linkedlist5.rb
@@ -20,22 +20,18 @@ def print_values(list_node)
 end
 
 def is_list_infinite(list_node)
-  tortoise = list_node
-  hare = list_node
-  while hare != nil
-      hare = hare.next_node     
-      if hare != nil 
-        hare = hare.next_node
-        tortoise = tortoise.next_node
-        return 1 if hare == tortoise        
-      end
+  tortoise = hare = list_node 
+  while hare && hare.next_node
+    hare = hare.next_node.next_node
+    tortoise = tortoise.next_node
+    return true if hare == tortoise        
   end
-  return 0
+  return false
 end
 
 node1 = LinkedListNode.new(37)
 node2 = LinkedListNode.new(99, node1)
 node3 = LinkedListNode.new(12, node2)
-#node1.next_node = node3
+node1.next_node = node3
 #print_values(node3)
 puts is_list_infinite(node3)
---
+++ script.rb
@@ -0,0 +1,5 @@
+add_numbers = lambda do |num1, num2|
+	return num1 + num2
+end
+
+puts add_numbers.call(3,4)
